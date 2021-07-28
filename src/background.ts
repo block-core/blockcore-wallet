6b897613ef028abd9453c5aafb451cc5e7a0ac98
Sondre <sondre@outlook.com>
2021-07-28 14:44:27 +0200

    Add background script

---
+++ src/background.ts
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.webNavigation.onCompleted.addListener(() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+        if (id) {
+          chrome.pageAction.show(id);
+        }
+      });
+    }, { url: [{ urlMatches: 'blockcore.net' }] });
+  });
