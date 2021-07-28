75f7a7e20b5a40f6b831771a2f7afefad25ea722
Sondre <sondre@outlook.com>
2021-07-28 17:12:07 +0200

    Update background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.webNavigation.onCompleted.addListener(() => {
-      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
-        if (id) {
-          chrome.pageAction.show(id);
-        }
-      });
-    }, { url: [{ urlMatches: 'blockcore.net' }] });
-  });
+  chrome.webNavigation.onCompleted.addListener(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+      if (id) {
+        chrome.pageAction.show(id);
+      }
+    });
+  }, { url: [{ urlMatches: 'blockcore.net' }] });
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('onMessage: ' + JSON.stringify(request));
+  sendResponse({ fromcontent: "This message is from content.js" });
+});
