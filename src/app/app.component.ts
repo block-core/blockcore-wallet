241d72abd5ab0d7b279416e786ddbd3ccbda14b7
Sondre <sondre@outlook.com>
2021-07-31 11:45:47 +0200

    WIP: Started working on the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApplicationState } from './services/application-state.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blockcore-extension';
+  wallet: any;
+
+  constructor(public appState: ApplicationState) {
+
+  }
 }
