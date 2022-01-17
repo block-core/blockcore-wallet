c056f6a51242a539271a374686863cc61a898eb8
SondreB <sondre@outlook.com>
2022-01-17 20:51:53 +0100

    Create addresses.ts
    - Add a list of address options.
    - These options should be listed depending on if the network supports it.

---
+++ src/background/addresses.ts
@@ -0,0 +1,37 @@
+export const addressFormats = [
+    {
+        name: 'Default',
+        purpose: 84,
+        specification: 'https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki'
+    },
+    {
+        name: 'Compatibility',
+        purpose: 49,
+        specification: 'https://github.com/bitcoin/bips/blob/master/bip-0049.mediawiki'
+    },
+    {
+        name: 'Legacy',
+        purpose: 44,
+        specification: 'https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki'
+    }
+]
+
+// export class Addresses {
+//     formats = [
+//         {
+//             name: 'Default',
+//             purpose: 84,
+//             specification: 'https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki'
+//         },
+//         {
+//             name: 'Compatibility',
+//             purpose: 49,
+//             specification: 'https://github.com/bitcoin/bips/blob/master/bip-0049.mediawiki'
+//         },
+//         {
+//             name: 'Legacy',
+//             purpose: 44,
+//             specification: 'https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki'
+//         }
+//     ]
+// }
