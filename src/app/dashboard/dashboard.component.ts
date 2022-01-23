df4383d0bcdd5bfddf015e592881e5dbc54aadda
SondreB <sondre@outlook.com>
2022-01-23 16:11:32 +0100

    Disable extra features
    - Display balance on the account listing (dashboard).

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,7 +12,8 @@ export interface Section {
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit, OnDestroy {
   mnemonic = '';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   version: "0.0.2",
-  features: ['wallet', 'identity', 'collectible', 'vault'],
+  // features: ['wallet', 'identity', 'collectible', 'vault'],
+  features: ['wallet'], // Preparing for alpha release, just hide features not ready for use.
   releaseUrl: 'https://github.com/block-core/blockcore-extension/releases',
   sourceUrl: 'https://github.com/block-core/blockcore-extension',
   instance: 'blockcore',
