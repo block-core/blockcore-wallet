202b4c2ca215d0b551136f69710eb54c6e986bd9
SondreB <sondre@outlook.com>
2022-01-28 02:00:19 +0100

    Hide some default account options

--- src/app/services/networks.service.ts
+++ src/app/services/networks.service.ts
@@ -60,7 +60,7 @@ export class NetworksService {
                         retrieved: null,
                         receive: [],
                         change: []
-                    },
+                    }
                 }, {
                     index: 0,
                     name: 'Cirrus',
@@ -74,50 +74,52 @@ export class NetworksService {
                         retrieved: null,
                         receive: [],
                         change: []
-                    },
-                }, {
-                    index: 0,
-                    name: 'City Coin',
-                    type: 'coin',
-                    network: 1926,
-                    purpose: 44,
-                    purposeAddress: 44,
-                    icon: 'paid',
-                    state: {
-                        balance: 0,
-                        retrieved: null,
-                        receive: [],
-                        change: []
-                    },
-                }, {
-                    index: 0,
-                    name: 'Identity',
-                    type: 'other',
-                    network: 616,
-                    purpose: 302,
-                    purposeAddress: 302,
-                    icon: 'account_circle',
-                    state: {
-                        balance: 0,
-                        retrieved: null,
-                        receive: [],
-                        change: []
-                    },
-                }, {
-                    index: 0,
-                    name: 'Nostr',
-                    type: 'other',
-                    network: 1237,
-                    purpose: 44,
-                    purposeAddress: 44, // TODO: Nostr should have custom derived address, add this ability (schnorr signature)
-                    icon: 'account_circle',
-                    state: {
-                        balance: 0,
-                        retrieved: null,
-                        receive: [],
-                        change: []
-                    },
-                }];
+                    }
+                }
+                    // , {
+                    //     index: 0,
+                    //     name: 'City Coin',
+                    //     type: 'coin',
+                    //     network: 1926,
+                    //     purpose: 44,
+                    //     purposeAddress: 44,
+                    //     icon: 'paid',
+                    //     state: {
+                    //         balance: 0,
+                    //         retrieved: null,
+                    //         receive: [],
+                    //         change: []
+                    //     },
+                    // }, {
+                    //     index: 0,
+                    //     name: 'Identity',
+                    //     type: 'other',
+                    //     network: 616,
+                    //     purpose: 302,
+                    //     purposeAddress: 302,
+                    //     icon: 'account_circle',
+                    //     state: {
+                    //         balance: 0,
+                    //         retrieved: null,
+                    //         receive: [],
+                    //         change: []
+                    //     },
+                    // }, {
+                    //     index: 0,
+                    //     name: 'Nostr',
+                    //     type: 'other',
+                    //     network: 1237,
+                    //     purpose: 44,
+                    //     purposeAddress: 44, // TODO: Nostr should have custom derived address, add this ability (schnorr signature)
+                    //     icon: 'account_circle',
+                    //     state: {
+                    //         balance: 0,
+                    //         retrieved: null,
+                    //         receive: [],
+                    //         change: []
+                    //     },
+                    // }
+                ];
                 break;
             case Environments.CoinVault:
                 accounts = [{
