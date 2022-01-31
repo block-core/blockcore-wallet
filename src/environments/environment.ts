95d70ef29bf017baddf3d210a95d7ea93325411c
SondreB <sondre@outlook.com>
2022-01-31 18:20:29 +0100

    Enable all features for dev mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   version: "0.0.5",
-  // features: ['wallet', 'identity', 'collectible', 'vault'],
-  features: ['wallet'], // Preparing for alpha release, just hide features not ready for use.
+  features: ['wallet', 'identity', 'collectible', 'vault'],
   releaseUrl: 'https://github.com/block-core/blockcore-extension/releases',
   sourceUrl: 'https://github.com/block-core/blockcore-extension',
   instance: 'blockcore',
