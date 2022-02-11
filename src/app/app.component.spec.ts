17b494fdcc74907480698dd09753c55be09f3525
SondreB <sondre@outlook.com>
2022-02-11 20:23:58 +0100

    Add import for Translation in Testbed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        TranslateModule.forRoot(),
       ],
       declarations: [
         AppComponent
       ],
+      providers: [TranslateService],
     }).compileComponents();
   });
 
