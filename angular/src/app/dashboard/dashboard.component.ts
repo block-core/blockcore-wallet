a08766f74dc6a13e7d074215303ba48ef65d0bb1
SondreB <sondre@outlook.com>
2022-02-25 18:47:13 +0100

    Turn on experimentalDecorators in the workspace settings
    - Avoid warning in VS Code.
    - Cleanup of code.

--- angular/src/app/dashboard/dashboard.component.ts
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
-import { Component, Inject, HostBinding, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
-import { CryptoService, UIState, FeatureService, LoggerService, NetworksService, NetworkStatusService, DebugLogService, WalletManager, SecureStateService } from '../services';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
+import {
+  CryptoService, UIState, FeatureService, LoggerService, NetworksService,
+  NetworkStatusService, DebugLogService, WalletManager, SecureStateService
+} from '../services';
 import { ActivatedRoute, Router } from '@angular/router';
 import { copyToClipboard } from '../shared/utilities';
 import { Observable } from 'rxjs';
 import { NetworkStatus } from '../interfaces';
-import { combineLatest } from 'rxjs';
 
 export interface Section {
   name: string;
@@ -43,32 +45,12 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
     this.uiState.showBackButton = false;
 
-    this.logger.info('Dashboard was opened');
-
     if (this.walletManager.activeWallet) {
       this.uiState.title = this.walletManager.activeWallet.name;
     }
-
-
-
-
   }
 
   ngOnInit(): void {
-    // EXAMPLE ON COLLECTING BOTH PARAMS AND DATA... left here if we ever need it.
-    // combineLatest(
-    //   this.route.params,
-    //   this.route.data,
-    //   (params: Params, data: Data) => ({
-    //     params,
-    //     data,
-    //   })
-    // ).subscribe((res: { params: Params; data: Data }) => {
-    //   const { params, data } = res;
-
-    //   this.getBook(params.id, data.uid);
-    // });
-
     this.activatedRoute.paramMap.subscribe(async params => {
 
       console.log('PARAMS:', params);
@@ -87,23 +69,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
           }
         }
       }
-
-      // const accountCount = this.walletManager.activeWallet?.accounts?.length;
-
-      // if (this.walletManager.activeWallet) {
-      //   // Check if the index is available before allowing to change.
-      //   if (accountId && accountCount != null) {
-      //     this.walletManager.activeWallet.activeAccountId = accountId;
-      //   }
-      //   else {
-      //     console.log('Attempting to show account that does not exists.');
-      //     this.router.navigateByUrl('/account');
-      //   }
-      // }
-      // else {
-      //   console.log('Attempting to show account when no wallet is selected.');
-      //   this.router.navigateByUrl('/');
-      // }
     });
 
     // TODO: activeWalletUnlocked IS FALSE ON FIRST UNLOCK!!!
--- angular/src/typings.d.ts
+++ angular/src/typings.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="chrome"/>
+
+declare module 'nostr-tools/nip06';
+declare module 'nostr-tools';
+declare module 'satcomma';
+declare module 'tiny-secp256k1';
