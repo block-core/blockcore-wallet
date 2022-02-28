1a5340be0a24f220624e79026b8bd4d14f3ce0dd
SondreB <sondre@outlook.com>
2022-02-28 16:57:55 +0100

    Preparing some setup for background indexing

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,4 +1,5 @@
 import { Message } from '../../angular/src/app/interfaces';
+import { IndexerBackgroundService } from './indexer';
 
 console.log('Extension: ServiceWorker script loaded');
 
@@ -14,8 +15,17 @@ chrome.runtime.onSuspend.addListener(() => {
 chrome.runtime.onInstalled.addListener(async ({ reason }) => {
     console.log('onInstalled', reason);
 
+    // Periodic alarm that will check if wallet should be locked.
     chrome.alarms.get('periodic', a => {
-        if (!a) chrome.alarms.create('periodic', { periodInMinutes: 0.5 });
+        if (!a) chrome.alarms.create('periodic', { periodInMinutes: 1 });
+    });
+
+    // The index alarm is used to perform background scanning of the 
+    // whole address space of all wallets. This will check used addresses
+    // that might have received transactions after used the first time.
+    // This will be called at a slow interval, time to be decided later.
+    chrome.alarms.get('index', a => {
+        if (!a) chrome.alarms.create('index', { periodInMinutes: 2 });
     });
 
     if (reason === 'install') {
@@ -51,42 +61,48 @@ chrome.runtime.onInstalled.addListener(async ({ reason }) => {
     }
 });
 
-chrome.alarms.onAlarm.addListener(async () => {
-    const storage = globalThis.chrome.storage as any;
+chrome.alarms.onAlarm.addListener(async (alarm: chrome.alarms.Alarm) => {
+    if (alarm.name === 'periodic') {
+        const storage = globalThis.chrome.storage as any;
 
-    // Get both "active" (Date) and timeout (number of minutes) from local settings.
-    const { active, timeout } = await chrome.storage.local.get(['active', 'timeout']);
+        // Get both "active" (Date) and timeout (number of minutes) from local settings.
+        const { active, timeout } = await chrome.storage.local.get(['active', 'timeout']);
 
-    // Reset storage if there is no 'active' state data.
-    if (!active) {
-        await storage.session.remove(['keys']);
-        // await storage.session.clear(); // Might be dramatic to clear to whole session storage?
-        console.log('There are no active value, session storage is cleared.');
-    } else {
-        // Parse the active date.
-        const timeoutDate = new Date(active);
-
-        // The reset date is current date minus the timeout.
-        var resetDate = new Date(new Date().valueOf() - timeout);
-
-        // console.log('resetDate: ', resetDate.toJSON());
-        // console.log('timeoutDate: ', timeoutDate.toJSON());
-
-        // Check of the timeout has been reached and clear if it has.
-        if (resetDate > timeoutDate) {
+        // Reset storage if there is no 'active' state data.
+        if (!active) {
             await storage.session.remove(['keys']);
             // await storage.session.clear(); // Might be dramatic to clear to whole session storage?
-            console.log('Timeout has been researched, session storage is cleared.');
+            console.log('There are no active value, session storage is cleared.');
+        } else {
+            // Parse the active date.
+            const timeoutDate = new Date(active);
 
-            chrome.runtime.sendMessage({ event: 'timeout' }, function (response) {
-                console.log('Extension:sendMessage:response:', response);
-            });
+            // The reset date is current date minus the timeout.
+            var resetDate = new Date(new Date().valueOf() - timeout);
+
+            // console.log('resetDate: ', resetDate.toJSON());
+            // console.log('timeoutDate: ', timeoutDate.toJSON());
+
+            // Check of the timeout has been reached and clear if it has.
+            if (resetDate > timeoutDate) {
+                await storage.session.remove(['keys']);
+                // await storage.session.clear(); // Might be dramatic to clear to whole session storage?
+                console.log('Timeout has been researched, session storage is cleared.');
+
+                chrome.runtime.sendMessage({ event: 'timeout' }, function (response) {
+                    console.log('Extension:sendMessage:response:', response);
+                });
+            }
         }
+    } else if ('index') {
+        // Process Wallets
+        // Get what addresses to watch from local storage.
+        // globalThis.chrome.storage.local.get('')
+        const indexer = new IndexerBackgroundService();
+        // indexer.process();
+
+
     }
-
-    // Process Wallets
-    
-
 });
 
 // chrome.runtime.onMessage.addListener((message, callback) => {
