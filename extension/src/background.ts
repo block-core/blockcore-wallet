86c668193a7d407972ac8e07f8552776f66e4bf2
SondreB <sondre@outlook.com>
2022-04-01 01:42:30 +0200

    Remove unused event handlers

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -40,34 +40,6 @@ async function getTabId() {
 //     }
 // });
 
-chrome.action.onClicked.addListener((tab) => {
-    console.log('ACTION!!', tab);
-
-    // var popups = chrome.extension.getViews({ type: "popup" });
-    // if (popups.length != 0) {
-    //     console.log('FOUND A POPUP!!');
-    //     var popup = popups[0];
-    //     console.log(popup);
-    //     // popup.doSomething();
-    // }
-    // else {
-    //     console.log('No POPUP!?!!');
-    // }
-
-    // const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    // const results = await chrome.scripting.executeScript({
-    //   target: {tabId: tab.id},
-
-
-});
-
-chrome.action.onClicked.addListener((tab) => {
-    console.log('The tab: ', tab);
-    console.log('YES!!!');
-});
-
-
-
 chrome.runtime.onInstalled.addListener(async ({ reason }) => {
     console.log('onInstalled', reason);
 
