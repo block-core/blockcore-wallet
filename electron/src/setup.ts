ca419a49d433897d0fa35ea04ea63fbf24f26a71
SondreB <sondre@outlook.com>
2022-05-01 23:21:51 +0200

    Don't use different CSP for dev or not

--- electron/src/setup.ts
+++ electron/src/setup.ts
@@ -217,15 +217,29 @@ export class ElectronCapacitorApp {
 }
 
 // Set a CSP up for our application based on the custom scheme
+// export function setupContentSecurityPolicy(customScheme: string): void {
+//   session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+//     callback({
+//       responseHeaders: {
+//         ...details.responseHeaders,
+//         'Content-Security-Policy': [
+//           electronIsDev
+//             ? `default-src ${customScheme}://* 'unsafe-inline' devtools://* 'unsafe-eval' data:`
+//             : `default-src ${customScheme}://* 'unsafe-inline' data:`,
+//         ],
+//       },
+//     });
+//   });
+// }
+
+// Set a CSP up for our application based on the custom scheme
 export function setupContentSecurityPolicy(customScheme: string): void {
   session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
     callback({
       responseHeaders: {
         ...details.responseHeaders,
         'Content-Security-Policy': [
-          electronIsDev
-            ? `default-src ${customScheme}://* 'unsafe-inline' devtools://* 'unsafe-eval' data:`
-            : `default-src ${customScheme}://* 'unsafe-inline' data:`,
+          `default-src ${customScheme}://* 'unsafe-inline' devtools://* 'unsafe-eval' data:`
         ],
       },
     });
