1ff4b637d17be087b227eaae1abbc9868abe166b
SondreB <sondre@outlook.com>
2022-05-04 13:26:49 +0200

    Add Electron setup for Blockcore Wallet
    - Does not load dependencies (css, js, etc) properly.

---
+++ electron/src/main.js
@@ -0,0 +1,63 @@
+import path from "path";
+import url from "url";
+import { app, Menu, ipcMain, shell } from "electron";
+import appMenuTemplate from "./menu/app_menu_template";
+import editMenuTemplate from "./menu/edit_menu_template";
+import devMenuTemplate from "./menu/dev_menu_template";
+import createWindow from "./helpers/window";
+
+import env from "env";
+
+if (env.name !== "production") {
+  const userDataPath = app.getPath("userData");
+  app.setPath("userData", `${userDataPath} (${env.name})`);
+}
+
+const setApplicationMenu = () => {
+  const menus = [appMenuTemplate, editMenuTemplate];
+
+  if (env.name !== "production") {
+    menus.push(devMenuTemplate);
+  }
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
+};
+
+const initIpc = () => {
+  ipcMain.on("need-app-path", (event, arg) => {
+    event.reply("app-path", app.getAppPath());
+  });
+  ipcMain.on("open-external-link", (event, href) => {
+    shell.openExternal(href);
+  });
+};
+
+app.on("ready", () => {
+  setApplicationMenu();
+  initIpc();
+
+  const mainWindow = createWindow("main", {
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  if (env.name === "development") {
+    mainWindow.openDevTools();
+  }
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
---
+++ electron/src/menu/app_menu_template.js
@@ -0,0 +1,14 @@
+import { app } from "electron";
+
+export default {
+  label: "File",
+  submenu: [
+    {
+      label: "Quit",
+      accelerator: "CmdOrCtrl+Q",
+      click: () => {
+        app.quit();
+      }
+    }
+  ]
+};
