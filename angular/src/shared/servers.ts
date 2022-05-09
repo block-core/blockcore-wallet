a6ce1599020d1b3ad1171234c70570447411e74e
SondreB <sondre@outlook.com>
2022-05-09 22:12:12 +0200

    Update CoinVault server domains

--- angular/src/shared/servers.ts
+++ angular/src/shared/servers.ts
@@ -1,9 +1,9 @@
 export const Servers: any = {
     group1: {
-        'BTC': ['https://btc.coinvault.io'],
-        'STRAX': ['https://strax.indexer.blockcore.net', 'https://strax.coinvault.io'],
+        'BTC': ['https://btc.indexer.coinvault.io'],
+        'STRAX': ['https://strax.indexer.blockcore.net', 'https://strax.indexer.coinvault.io'],
         'TSTRAX': ['https://tstrax.indexer.blockcore.net'],
-        'CRS': ['https://crs.indexer.blockcore.net', 'https://crs.coinvault.io'],
+        'CRS': ['https://crs.indexer.blockcore.net', 'https://crs.indexer.coinvault.io'],
         'TCRS': ['https://tcrs.indexer.blockcore.net'],
         'CITY': ['https://city.indexer.blockcore.net'],
     },
