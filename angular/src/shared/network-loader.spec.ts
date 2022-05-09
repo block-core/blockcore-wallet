dd51d11dd506a04d6f80c52c4e5287321c6dbd17
SondreB <sondre@outlook.com>
2022-05-09 22:31:53 +0200

    Add integration test to verify all group1 indexers

--- angular/src/shared/network-loader.spec.ts
+++ angular/src/shared/network-loader.spec.ts
@@ -1,6 +1,8 @@
 import { NetworkLoader } from "./network-loader";
+import { Servers } from "./servers";
+const axios = require('axios');
 
-describe('AddressManager', () => {
+describe('NetworkLoader', () => {
     beforeEach(() => { });
 
     it('Should return random server', () => {
@@ -8,11 +10,11 @@ describe('AddressManager', () => {
         const loader = new NetworkLoader();
 
         const servers = [
-            loader.getServer('BTC', 'group1'),
-            loader.getServer('BTC', 'group1'),
-            loader.getServer('BTC', 'group1'),
-            loader.getServer('BTC', 'group1'),
-            loader.getServer('BTC', 'group1')
+            loader.getServer('STRAX', 'group1'),
+            loader.getServer('STRAX', 'group1'),
+            loader.getServer('STRAX', 'group1'),
+            loader.getServer('STRAX', 'group1'),
+            loader.getServer('STRAX', 'group1')
         ];
 
         // Get unique list of servers.
@@ -20,6 +22,26 @@ describe('AddressManager', () => {
 
         // Expect more than a si1ngle one (then random has not worked well).
         expect(unique.length > 1).toBeTrue();
+    });
+
+    it('All indexers in group1 should respond', async () => {
+        const servers = Servers.group1;
+        const entries = Object.values(servers);
+
+        for (var i = 0; i < entries.length; i++) {
+            const entry = entries[i] as any;
+
+            for (var j = 0; j < entry.length; j++) {
+                const server = entry[j];
+
+                try {
+                    var result = await axios.get(server + '/api/stats');
+                    expect(result.status).toBe(200);
+                } catch (error) {
+                    throw new Error('Failed to verify indexer: ' + server);
+                }
+            }
+        }
     });
 
 });
