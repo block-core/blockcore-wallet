bab1b06073f3885ac54ae993e12455c610c09403
SondreB <sondre@outlook.com>
2022-05-21 22:51:13 +0200

    Update the list of available servers

--- angular/src/shared/servers.ts
+++ angular/src/shared/servers.ts
@@ -1,16 +1,14 @@
 export const Servers: any = {
     group1: {
         'BTC': ['https://btc.indexer.coinvault.io'],
-        //'STRAX': ['https://strax.indexer.blockcore.net', 'https://strax.indexer.coinvault.io'],
-        'STRAX': ['https://strax.indexer.blockcore.net'],
+        'STRAX': ['https://strax.indexer.blockcore.net', 'https://strax.indexer.coinvault.io', , 'https://strax.indexer.thedude.pro'],
         'TSTRAX': ['https://tstrax.indexer.blockcore.net'],
-        //'CRS': ['https://crs.indexer.blockcore.net', 'https://crs.indexer.coinvault.io'],
-        'CRS': ['https://crs.indexer.blockcore.net'],
+        'CRS': ['https://crs.indexer.blockcore.net', 'https://crs.indexer.coinvault.io', 'https://crs.indexer.thedude.pro'],
         'TCRS': ['https://tcrs.indexer.blockcore.net'],
         'CITY': ['https://city.indexer.blockcore.net'],
     },
     group2: {
-        'BTC': ['btc.indexer.coinvault.io'],
+        'BTC': ['https://btc.indexer.coinvault.io'],
         'STRAX': ['https://strax.indexer.blockcore.net'],
         'TSTRAX': ['https://tstrax.indexer.blockcore.net'],
         'CRS': ['https://crs.indexer.blockcore.net'],
@@ -18,7 +16,7 @@ export const Servers: any = {
         'CITY': ['https://city.indexer.blockcore.net'],
     },
     group3: {
-        'BTC': ['btc.indexer.coinvault.io'],
+        'BTC': ['https://btc.indexer.coinvault.io'],
         'STRAX': ['https://strax.indexer.blockcore.net'],
         'TSTRAX': ['https://tstrax.indexer.blockcore.net'],
         'CRS': ['https://crs.indexer.blockcore.net'],
