21ab0c401cb61d2e25e70a2847a9f46c0aa10c82
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2022-06-01 15:25:47 +0430

    Add x42 network (#142)
    * Add x42 network
    
    * Fix some mistakes

---
+++ angular/src/shared/networks/X42.ts
@@ -0,0 +1,25 @@
+import { Network } from './network';
+
+export class X42 implements Network {
+    id: string = 'X42';
+    name: string = 'X42';
+    symbol = 'x42';
+    network: number = 424242;
+    purpose: number = 84;
+    messagePrefix = '\x18Bitcoin Signed Message:\n';
+    bech32 = 'X';
+    bip32 = {
+        public: 0x0488b21e,
+        private: 0x0488ade4,
+    };
+    pubKeyHash = 75;
+    scriptHash = 125;
+    wif = 0x08;
+    feeRate = 0;
+    minFeeRate = 0;
+    testnet = false;
+    singleAddress = true;
+    SmartContractSupport = false;
+    isProofOfStake = true;
+
+}
