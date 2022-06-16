268bcea560b15c2d7aafc860e5715b5e5ae3812b
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2022-06-16 13:15:26 +0430

    Add multiple servers to the indexers list (#172)
    https://city.indexer.seniorblockchain.io
    https://x42.indexer.seniorblockchain.io

--- angular/src/shared/servers.ts
+++ angular/src/shared/servers.ts
@@ -5,11 +5,11 @@ export const Servers: any = {
         'TSTRAX': ['https://tstrax.indexer.blockcore.net'],
         'CRS': ['https://crs.indexer.blockcore.net', 'https://crs.indexer.coinvault.io', 'https://crs.indexer.thedude.pro'],
         'TCRS': ['https://tcrs.indexer.blockcore.net'],
-        'CITY': ['https://city.indexer.blockcore.net'],
+        'CITY': ['https://city.indexer.blockcore.net', 'https://city.indexer.seniorblockchain.io'],
         'CYBITS': ['https://cybits.indexer.blockcore.net', 'https://cybits.indexer.seniorblockchain.io'],
         'RSC': ['https://rsc.indexer.blockcore.net', 'https://rsc.indexer.seniorblockchain.io'],
         'SBC': ['https://sbc.indexer.blockcore.net', 'https://sbc.indexer.seniorblockchain.io'],
-        'X42': ['https://x42.indexer.blockcore.net'],
+        'X42': ['https://x42.indexer.blockcore.net', 'https://x42.indexer.seniorblockchain.io'],
     },
     group2: {
         'BTC': ['https://btc.indexer.coinvault.io'],
@@ -17,7 +17,7 @@ export const Servers: any = {
         'TSTRAX': ['https://tstrax.indexer.blockcore.net'],
         'CRS': ['https://crs.indexer.blockcore.net'],
         'TCRS': ['https://tcrs.indexer.blockcore.net'],
-        'CITY': ['https://city.indexer.blockcore.net'],
+        'CITY': ['https://city.indexer.blockcore.net', 'https://city.indexer.seniorblockchain.io'],
     },
     group3: {
         'BTC': ['https://btc.indexer.coinvault.io'],
@@ -25,6 +25,6 @@ export const Servers: any = {
         'TSTRAX': ['https://tstrax.indexer.blockcore.net'],
         'CRS': ['https://crs.indexer.blockcore.net'],
         'TCRS': ['https://tcrs.indexer.blockcore.net'],
-        'CITY': ['https://city.indexer.blockcore.net'],
+        'CITY': ['https://city.indexer.blockcore.net', 'https://city.indexer.seniorblockchain.io'],
     }
 };
