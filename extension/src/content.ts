5d58c4e8f1302d53287fdb7dd9892eb7caea20bb
SondreB <sondre@outlook.com>
2022-06-21 01:14:27 +0200

    Fix loading of content and provider scripts

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,55 +1,49 @@
 // If the blockcore is not registered yet, load the provider now.
 if (!globalThis.blockcore) {
-  const providerUrl = chrome.runtime.getURL("provider.js");
-
   // Load the JavaScript provided by the extension. We need this to activate the extension.
-  var script = document.createElement("script");
+  let script = document.createElement('script');
   script.setAttribute('async', 'false');
   script.setAttribute('type', 'text/javascript');
-  script.setAttribute('src', providerUrl)
+  script.setAttribute('src', chrome.runtime.getURL('provider.js'));
+  document.head.appendChild(script);
 
   script.onload = function () {
     console.log('Blockcore Provider Script Loaded from Extension.');
   };
 
-  document.head.appendChild(script);
+  // listen for messages from the provider script
+  window.addEventListener('message', async (message) => {
+    if (message.source !== window) return;
+    if (!message.data) return;
+    if (!message.data.params) return;
+    if (message.data.extension !== 'blockcore') return;
+
+    var response;
+    try {
+      console.log('SENDING MESSAGE!!');
+      response = await chrome.runtime.sendMessage({
+        type: message.data.type,
+        params: message.data.params,
+        extension: 'blockcore',
+        source: 'content',
+        target: 'tabs', // tabs = extension windows, popups, tabs.
+        host: location.host,
+      });
+    } catch (error) {
+      response = { error };
+    }
+
+    window.postMessage(
+      {
+        id: message.data.id,
+        type: message.data.type,
+        extension: 'blockcore',
+        source: 'content',
+        target: 'provider',
+        host: location.host,
+        response,
+      },
+      message.origin
+    );
+  });
 }
-
-// listen for messages from the provider script
-globalThis.addEventListener('message', async message => {
-
-  if (message.source !== window || !message.data || !message.data.ext) {
-    return;
-  }
-
-  if (message.data.source !== 'provider') {
-    return;
-  };
-
-  try {
-    chrome.runtime.sendMessage({
-      id: message.data.id,
-      type: message.data.type,
-      data: message.data.data,
-      ext: 'blockcore', // Don't read what is provided by provider script.
-      source: 'content', // Don't read what is provided by provider script.
-      target: 'tabs', // Don't read what is provided by provider script.
-      host: location.host
-    }, (response) => {
-
-      // If there was no response provided, set the response to last error.
-      if (!response) {
-        // We get this exception if we attempt to serialize the whole lastError object (even though it is a basic object?):
-        // Error handling response: Error: Failed to execute 'postMessage' on 'Window': An object could not be cloned.
-        response = { error: { message: chrome.runtime.lastError.message } };
-      }
-
-      // return response and complete the promise.
-      window.postMessage({ id: message.data.id, type: message.data.type, ext: 'blockcore', target: 'provider', source: 'content', response }, message.origin);
-    });
-  }
-  catch (error) {
-    console.error(error);
-    window.postMessage({ id: message.data.id, type: message.data.type, ext: 'blockcore', target: 'provider', source: 'content', response: { error: { message: error.message } } }, message.origin);
-  }
-})
--- extension/src/provider.ts
+++ extension/src/provider.ts
@@ -1,141 +1,142 @@
 // Register global provider for Blockcore:
 globalThis.blockcore = {
-    _promises: {},
+  _promises: {},
 
-    _call(type, data) {
-        return new Promise((resolve, reject) => {
-            const id = v4();
-            this._promises[id] = { resolve, reject };
+  _call(type, data) {
+    return new Promise((resolve, reject) => {
+      const id = v4();
+      this._promises[id] = { resolve, reject };
 
-            globalThis.postMessage({ type, id, data, source: 'provider', target: 'tabs', ext: 'blockcore' }, '*');
-        });
-    },
+      globalThis.postMessage({ type, id, data, source: 'provider', target: 'tabs', ext: 'blockcore' }, '*');
+    });
+  },
 
-    connect: (callback) => {
+  connect: (callback) => {},
+  open: () => {
+    console.log('OPEN WAS CALLED!');
+    console.log('chrome.runtime:', chrome.runtime);
 
-    },
-    open: () => {
-        chrome.runtime.sendMessage({ greeting: "hello from provider" }, function (response) {
-            // console.log(response.farewell);
-        });
+    chrome.runtime.sendMessage({ greeting: 'hello from provider' }, function (response) {
+        console.log('runtime.sendMessage!');
+      // console.log(response.farewell);
+    });
 
-        chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-            console.log('onMessage (PROVIDER): ' + JSON.stringify(request));
-            sendResponse({ fromcontent: "This message is from provider.js" });
-        });
-
-    },
-    getAccounts: () => {
-        console.log('Getting accounts!');
-    },
-    on: (event, callback) => {
-
-    },
-    async getPublicKey() {
-        return this._call('ext-getpublickey', null);
-    },
-    async login() {
-        return this._call('ext-login', { ok: false });
-    },
-    // loadScript: (url) => {
-    //     var script = document.createElement("script");
-    //     script.src = url;
-    //     document.head.appendChild(script);
-    // }
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+      console.log('onMessage (PROVIDER): ' + JSON.stringify(request));
+      sendResponse({ fromcontent: 'This message is from provider.js' });
+    });
+  },
+  getAccounts: () => {
+    console.log('Getting accounts!');
+  },
+  on: (event, callback) => {},
+  async getPublicKey() {
+    return this._call('ext-getpublickey', null);
+  },
+  async login() {
+    return this._call('ext-login', { ok: false });
+  },
+  // loadScript: (url) => {
+  //     var script = document.createElement("script");
+  //     script.src = url;
+  //     document.head.appendChild(script);
+  // }
 };
 
 // This will receive various messages that are posted to the window. Make sure we filter out anything that
 // is not related to the extension.
-globalThis.addEventListener('message', message => {
-
+globalThis.addEventListener('message', (message) => {
   //console.log(message);
 
-    // Make sure there is data, extension is setup and it belongs to the existing promises in this web app.
-    if (!message.data || !message.data.ext || !message.data.response || !globalThis.blockcore._promises[message.data.id]) {
-        return;
-    }
+  // Make sure there is data, extension is setup and it belongs to the existing promises in this web app.
+  if (!message.data || !message.data.ext || !message.data.response || !globalThis.blockcore._promises[message.data.id]) {
+    return;
+  }
 
-    // It is possible that calls to the extension is returned without handled by an instance of the extension,
-    // if that happens, then response will be undefined.
+  // It is possible that calls to the extension is returned without handled by an instance of the extension,
+  // if that happens, then response will be undefined.
 
-    //console.log('globalThis.addEventListener (PROVIDER), HANDLE THIS MESSAGE: ', message);
+  //console.log('globalThis.addEventListener (PROVIDER), HANDLE THIS MESSAGE: ', message);
   console.log('DATA =: ', message.data);
 
-    if (message.data.response?.error) {
-        let error = new Error('blockcore: ' + message.data.response.error.message);
-        error.stack = message.data.response.error.stack;
-        globalThis.blockcore._promises[message.data.id].reject(error);
-    } else {
-        globalThis.blockcore._promises[message.data.id].resolve(message.data.response);
-    }
+  if (message.data.response?.error) {
+    let error = new Error('blockcore: ' + message.data.response.error.message);
+    error.stack = message.data.response.error.stack;
+    globalThis.blockcore._promises[message.data.id].reject(error);
+  } else {
+    globalThis.blockcore._promises[message.data.id].resolve(message.data.response);
+  }
 });
 
 // TODO: Only override the nostr handler when user has decided that this should happen!
 globalThis.nostr = {
-    _requests: {},
-    _pubkey: null,
+  _requests: {},
+  _pubkey: null,
 
-    async getPublicKey() {
-        if (this._pubkey) return this._pubkey
-        this._pubkey = await this._call('getPublicKey', {})
-        return this._pubkey
+  async getPublicKey() {
+    if (this._pubkey) return this._pubkey;
+    this._pubkey = await this._call('getPublicKey', {});
+    return this._pubkey;
+  },
+
+  async signEvent(event) {
+    return this._call('signEvent', { event });
+  },
+
+  async getRelays() {
+    return this._call('getRelays', {});
+  },
+
+  nip04: {
+    async encrypt(peer, plaintext) {
+      return globalThis.nostr._call('nip04.encrypt', { peer, plaintext });
     },
 
-    async signEvent(event) {
-        return this._call('signEvent', { event })
+    async decrypt(peer, ciphertext) {
+      return globalThis.nostr._call('nip04.decrypt', { peer, ciphertext });
     },
+  },
 
-    async getRelays() {
-        return this._call('getRelays', {})
-    },
-
-    nip04: {
-        async encrypt(peer, plaintext) {
-            return globalThis.nostr._call('nip04.encrypt', { peer, plaintext })
+  _call(type, params) {
+    return new Promise((resolve, reject) => {
+      let id = Math.random().toString().slice(4);
+      this._requests[id] = { resolve, reject };
+      window.postMessage(
+        {
+          id,
+          ext: 'nos2x',
+          type,
+          params,
         },
-
-        async decrypt(peer, ciphertext) {
-            return globalThis.nostr._call('nip04.decrypt', { peer, ciphertext })
-        }
-    },
-
-    _call(type, params) {
-        return new Promise((resolve, reject) => {
-            let id = Math.random().toString().slice(4)
-            this._requests[id] = { resolve, reject }
-            window.postMessage(
-                {
-                    id,
-                    ext: 'nos2x',
-                    type,
-                    params
-                },
-                '*'
-            )
-        })
-    }
-}
+        '*'
+      );
+    });
+  },
+};
 
 /** Taken from here to avoid dependency on UUID package in the provider.ts: https://github.com/decentralized-identity/web-extension/blob/master/extension/js/modules/uuid.js */
 function randomBytes(length = 16, format) {
-    let bytes = crypto.getRandomValues(new Uint8Array(length));
-    switch (format) {
-        case 'raw': return bytes;
-        case 'base64Url': return;
-        default: return bytes.join('');
-    }
-};
+  let bytes = crypto.getRandomValues(new Uint8Array(length));
+  switch (format) {
+    case 'raw':
+      return bytes;
+    case 'base64Url':
+      return;
+    default:
+      return bytes.join('');
+  }
+}
 
 function v4() {
-    function getRandomSymbol(symbol) {
-        var array;
-        if (symbol === 'y') {
-            array = ['8', '9', 'a', 'b'];
-            return array[Math.floor(Math.random() * array.length)];
-        }
-        array = new Uint8Array(1);
-        crypto.getRandomValues(array);
-        return (array[0] % 16).toString(16);
+  function getRandomSymbol(symbol) {
+    var array;
+    if (symbol === 'y') {
+      array = ['8', '9', 'a', 'b'];
+      return array[Math.floor(Math.random() * array.length)];
     }
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, getRandomSymbol);
-};
+    array = new Uint8Array(1);
+    crypto.getRandomValues(array);
+    return (array[0] % 16).toString(16);
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, getRandomSymbol);
+}
