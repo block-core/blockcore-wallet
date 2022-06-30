1a25b803d6c6107d687f4c89cf75578782a3d03a
SondreB <sondre@outlook.com>
2022-07-01 01:02:24 +0200

    @HostListener added to individual components to execute on window close

--- angular/src/app/action/identity/identity.component.ts
+++ angular/src/app/action/identity/identity.component.ts
@@ -1,39 +1,35 @@
-import { Component, Inject, HostBinding, ChangeDetectorRef, ApplicationRef, NgZone } from '@angular/core';
+import { Component, Inject, HostBinding, ChangeDetectorRef, ApplicationRef, NgZone, HostListener } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CryptoService, NetworksService, AppManager, WalletManager, UIState } from '../../services';
 import { Router } from '@angular/router';
 import { ActionService } from 'src/app/services/action.service';
 
 @Component({
-    selector: 'app-identity',
-    templateUrl: './identity.component.html',
-    styleUrls: ['./identity.component.css']
+  selector: 'app-identity',
+  templateUrl: './identity.component.html',
+  styleUrls: ['./identity.component.css'],
 })
 export class ActionIdentityComponent {
-    content?: string;
+  content?: string;
 
-    constructor(
-        public uiState: UIState,
-        private crypto: CryptoService,
-        private router: Router,
-        private app: ApplicationRef,
-        private ngZone: NgZone,
-        private action: ActionService,
-        public networkService: NetworksService,
-        private manager: AppManager,
-        public walletManager: WalletManager,
-        private cd: ChangeDetectorRef) {
-        this.uiState.title = 'Action: Identity';
+  constructor(public uiState: UIState, private crypto: CryptoService, private router: Router, private app: ApplicationRef, private ngZone: NgZone, private action: ActionService, public networkService: NetworksService, private manager: AppManager, public walletManager: WalletManager, private cd: ChangeDetectorRef) {
+    this.uiState.title = 'Action: Identity';
 
-        this.content = 'The website is requesting you to generate an DID Document.'; // this.uiState.action?.document;
-    }
+    this.content = 'The website is requesting you to generate an DID Document.'; // this.uiState.action?.document;
+  }
 
-    sign() {
-        // this.manager.sign(this.content, this.uiState.action?.tabId);
-        window.close();
-    }
+  // This attribute must be added to all individual components, important for closing with X to register 'no' reply.
+  @HostListener('window:beforeunload')
+  rejectDialog() {
+    this.action.authorize('no');
+  }
 
-    exit() {
-        this.action.clearAction();
-    }
+  sign() {
+    // this.manager.sign(this.content, this.uiState.action?.tabId);
+    window.close();
+  }
+
+  exit() {
+    this.action.clearAction();
+  }
 }
