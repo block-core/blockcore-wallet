10b8298c7ca24b6756d43442551fa20870ce400b
SondreB <sondre@outlook.com>
2022-07-01 00:58:20 +0200

    Move logic into ActionService

--- angular/src/app/services/action.service.ts
+++ angular/src/app/services/action.service.ts
@@ -1,42 +1,65 @@
-import { Injectable } from "@angular/core";
-import { Action, ActionStore } from "src/shared";
+import { Injectable } from '@angular/core';
+import { Action, ActionMessageResponse, ActionStore } from 'src/shared';
+import { UIState } from './ui-state.service';
+import { WalletManager } from './wallet-manager';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ActionService {
-    constructor(private store: ActionStore) {
+  constructor(private store: ActionStore, public uiState: UIState, public walletManager: WalletManager) {}
 
+  app: string;
+  content: string;
+
+  async clearAction() {
+    this.store.set(undefined);
+    await this.store.save();
+  }
+
+  async setAction(data: Action, broadcast: boolean) {
+    if (typeof data.action !== 'string') {
+      console.error('Only objects that are string are allowed as actions.');
+      return;
     }
 
-    app: string;
-    content: string;
-
-    async clearAction() {
-        this.store.set(undefined);
-        await this.store.save();
+    if (data.document != null && typeof data.document !== 'string') {
+      console.error('Only objects that are string are allowed as actions.');
+      return;
     }
 
-    async setAction(data: Action, broadcast: boolean) {
-        if (typeof data.action !== 'string') {
-            console.error('Only objects that are string are allowed as actions.');
-            return;
-        }
+    this.store.set(data);
 
-        if (data.document != null && typeof data.document !== 'string') {
-            console.error('Only objects that are string are allowed as actions.');
-            return;
-        }
+    await this.store.save();
 
-        this.store.set(data);
+    // if (broadcast) {
+    //     this.broadcastState();
+    // }
 
-        await this.store.save();
+    // Raise this after state has been updated, so orchestrator in UI can redirect correctly.
+    // this.communication.sendToAll('action-changed', this.state.action);
+  }
 
-        // if (broadcast) {
-        //     this.broadcastState();
-        // }
+  authorize(permission: string) {
+    // Reset params so the action can be re-triggered.
+    this.uiState.params = null;
 
-        // Raise this after state has been updated, so orchestrator in UI can redirect correctly.
-        // this.communication.sendToAll('action-changed', this.state.action);
-    }
+    const reply: ActionMessageResponse = {
+      prompt: true, // This indicates that message comes from the popup promt.
+      target: 'provider',
+      src: 'tabs',
+      ext: 'blockcore',
+      permission: permission,
+      args: ['cipher'],
+      id: this.uiState.action.id,
+      action: this.uiState.action.action,
+      app: this.uiState.action.app,
+      walletId: this.walletManager.activeWalletId,
+      accountId: this.walletManager.activeAccountId,
+    };
+
+    // TODO: Move this to a communication service.
+    // Inform the provider script that user has signed the data.
+    chrome.runtime.sendMessage(reply);
+  }
 }
