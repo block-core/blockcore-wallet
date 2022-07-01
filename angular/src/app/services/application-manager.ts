ee30f6fe6ac866db3fba78deb8f7c36e0e33a84b
SondreB <sondre@outlook.com>
2022-07-01 23:42:37 +0200

    Remove the external communication service
    - No longer in use

--- angular/src/app/services/application-manager.ts
+++ angular/src/app/services/application-manager.ts
@@ -1,57 +1,40 @@
-import { Injectable } from "@angular/core";
-import { MINUTE } from "../shared/constants";
-import {
-    SecureStateService,
-    CryptoUtility, DataSyncService, NetworkLoader,
-  CommunicationService, WalletManager, CommunicationExternal
-} from "./";
-import { StateService } from "./state.service";
+import { Injectable } from '@angular/core';
+import { MINUTE } from '../shared/constants';
+import { SecureStateService, CryptoUtility, DataSyncService, NetworkLoader, CommunicationService, WalletManager } from './';
+import { StateService } from './state.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 /** Main logic that is responsible for orchestration of the app. */
 export class AppManager {
-    constructor(
-        public sync: DataSyncService,
-        public state: StateService,
-        public walletManager: WalletManager,
-      public communication: CommunicationService,
-      public communicationExternal: CommunicationExternal,
-        public networkLoader: NetworkLoader,
-        public crypto: CryptoUtility,
-        public secure: SecureStateService
-    ) {
+  constructor(public sync: DataSyncService, public state: StateService, public walletManager: WalletManager, public communication: CommunicationService, public networkLoader: NetworkLoader, public crypto: CryptoUtility, public secure: SecureStateService) {}
 
-    }
+  async initialize() {
+    await this.communication.initialize();
 
-    async initialize() {
-        await this.communication.initialize();
+    // Load all the stores.
+    await this.state.load();
 
-        await this.communicationExternal.initialize();
+    // Then load the secure state.
+    await this.secure.load();
 
-        // Load all the stores.
-        await this.state.load();
+    // Reset the reset timer
+    this.walletManager.resetTimer();
 
-        // Then load the secure state.
-        await this.secure.load();
+    this.scheduledWatcher();
+  }
 
-        // Reset the reset timer
-        this.walletManager.resetTimer();
+  /** The watcher will query the latest receive/change address more frequently than full wallet scan. This method will
+   * send a message to the service worker (background) once every 5 minutes if the UI is still active, to ensure that the watch
+   * timer is active in the service worker. The service worker can be killed at any moment, so this ensures that it comes
+   * back to live. */
+  scheduledWatcher() {
+    setInterval(() => {
+      this.communication.send(this.communication.createMessage('keep-alive', {}, 'background'));
+    }, MINUTE * 5);
 
-        this.scheduledWatcher();
-    };
-
-    /** The watcher will query the latest receive/change address more frequently than full wallet scan. This method will
-     * send a message to the service worker (background) once every 5 minutes if the UI is still active, to ensure that the watch
-     * timer is active in the service worker. The service worker can be killed at any moment, so this ensures that it comes
-     * back to live. */
-    scheduledWatcher() {
-        setInterval(() => {
-            this.communication.send(this.communication.createMessage('keep-alive', {}, 'background'));
-        }, MINUTE * 5);
-
-        // Activate the watch right away.
-        this.communication.send(this.communication.createMessage('keep-alive', {}, 'background'));
-    }
+    // Activate the watch right away.
+    this.communication.send(this.communication.createMessage('keep-alive', {}, 'background'));
+  }
 }
