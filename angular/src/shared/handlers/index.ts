2dde08c2b27736014752fd7cd52517d6df175311
SondreB <sondre@outlook.com>
2022-08-16 23:05:27 +0200

    Disable all actions other than 'signMessage'

--- angular/src/shared/handlers/index.ts
+++ angular/src/shared/handlers/index.ts
@@ -8,16 +8,16 @@ import { SignMessageHandler } from './sign-message-handler';
 export class Handlers {
   static getAction(action: string): ActionHandler {
     switch (action) {
-      case 'sign': // Allows signing any arbitrary bytes without prefix. This can be abused easily and is "dangerous" for users to use. Can be used to sign transactions.
-        return new SignHandler();
+      // case 'sign': // Allows signing any arbitrary bytes without prefix. This can be abused easily and is "dangerous" for users to use. Can be used to sign transactions.
+      //   return new SignHandler();
       case 'signMessage': // Signing using a message prefix specific for the network the account belongs to. More secure and cannot be used to sign transactions.
         return new SignMessageHandler();
-      case 'signVerifiableCredential': // Signing of Verifiable Credential, JSON encoded as signed JSON Web Token.
-        return new SignVerifiableCredentialHandler();
-      case 'signTypedData': // Not implemented yet, will be inspired by EIP-712: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md
-        return null;
-      case 'publicKey':
-        return new PublicKeyHandler();
+      // case 'signVerifiableCredential': // Signing of Verifiable Credential, JSON encoded as signed JSON Web Token.
+      //   return new SignVerifiableCredentialHandler();
+      // case 'signTypedData': // Not implemented yet, will be inspired by EIP-712: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md
+      //   return null;
+      // case 'publicKey':
+      //   return new PublicKeyHandler();
       default:
         return null;
     }
