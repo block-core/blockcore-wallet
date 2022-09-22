7729a89bc4cbf13526ccf4aa31c63b8906ed9bee
SondreB <sondre@outlook.com>
2022-09-22 19:05:32 +0200

    Automatically select the last account when one account is removed

--- angular/src/app/account/remove/remove.component.ts
+++ angular/src/app/account/remove/remove.component.ts
@@ -1,27 +1,19 @@
 import { Component, Inject, HostBinding } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common'
+import { Location } from '@angular/common';
 import { LoggerService, UIState, WalletManager } from '../../services';
 
 @Component({
   selector: 'app-account-remove',
   templateUrl: './remove.component.html',
-  styleUrls: ['../account.component.css']
+  styleUrls: ['../account.component.css'],
 })
 export class AccountRemoveComponent {
-  constructor(
-    private router: Router,
-    private location: Location,
-    public uiState: UIState,
-    private logger: LoggerService,
-    public walletManager: WalletManager,
-    private activatedRoute: ActivatedRoute,
-  ) {
+  constructor(private router: Router, private location: Location, public uiState: UIState, private logger: LoggerService, public walletManager: WalletManager, private activatedRoute: ActivatedRoute) {
     this.uiState.title = 'Remove Account';
 
-    this.activatedRoute.paramMap.subscribe(async params => {
-
+    this.activatedRoute.paramMap.subscribe(async (params) => {
       const accountId: any = params.get('index');
       const accountCount = this.walletManager.activeWallet?.accounts?.length;
 
@@ -29,13 +21,11 @@ export class AccountRemoveComponent {
         // Check if the index is available before allowing to change.
         if (accountId && accountCount != null) {
           // this.walletManager.activeWallet.activeAccountId = accountId;
-        }
-        else {
+        } else {
           this.logger.warn('Attempting to show account that does not exists.');
           this.router.navigateByUrl('/account');
         }
-      }
-      else {
+      } else {
         this.logger.warn('Attempting to show account when no wallet is selected.');
         this.router.navigateByUrl('/');
       }
@@ -50,6 +40,12 @@ export class AccountRemoveComponent {
     var activeWallet = this.walletManager.activeWallet;
 
     await this.walletManager.removeAccount(activeWallet.id, this.walletManager.activeAccountId);
+
+    // Select the last account whenever an account has been removed.
+    if (activeWallet.accounts.length > 0) {
+      const account = activeWallet.accounts[activeWallet.accounts.length - 1];
+      await this.walletManager.setActiveAccount(account.identifier);
+    }
 
     this.router.navigateByUrl('/dashboard');
   }
