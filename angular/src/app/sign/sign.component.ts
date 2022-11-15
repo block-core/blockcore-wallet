6c77f3e7f5015b9c0fcff0154e12c49894c3edd5
missmeowness <lbarvinskayaa@gmail.com>
2022-11-15 17:22:12 +0300

    Add Account selection

--- angular/src/app/sign/sign.component.ts
+++ angular/src/app/sign/sign.component.ts
@@ -1,21 +1,41 @@
-import { Component, Inject, HostBinding } from '@angular/core';
+import * as bitcoinMessage from 'bitcoinjs-message';
+import { Component, Inject, HostBinding, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { UIState } from '../services/ui-state.service';
-import { StateService } from '../services';
+import { StateService, WalletManager } from '../services';
 import { RuntimeService } from '../../shared/runtime.service';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { warn } from 'console';
+import { Account } from 'src/shared';
+import { Network } from 'src/shared/networks';
+import { HDKey } from '@scure/bip32';
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
   styleUrls: ['./sign.component.css'],
 })
-export class SignComponent {
+export class SignComponent implements OnInit {
   signFormGroup!: UntypedFormGroup;
-  constructor(private location: Location, public uiState: UIState, private state: StateService, private runtime: RuntimeService, private fb: UntypedFormBuilder) {
+  accounts: Account[];
+  constructor(private location: Location, public uiState: UIState, private state: StateService, private runtime: RuntimeService, private fb: UntypedFormBuilder, private walletManager: WalletManager) {
     this.uiState.title = 'Sign & Verify';
     this.uiState.showBackButton = true;
     this.uiState.goBackHome = true;
     this.signFormGroup = this.fb.group({ accountCtrl: ['From Russia with Love'], messageCtrl: ['From Russia with Love 2'] });
+  }
+  ngOnInit() {
+    this.accounts = this.walletManager.activeWallet.accounts;
+    this.signFormGroup.controls['accountCtrl'].setValue (this.walletManager.activeAccountId);
+  }
+  signData(network: Network, node: HDKey, content: string) {
+    var signature = bitcoinMessage.sign(content, Buffer.from(node.privateKey), true, network.messagePrefix);
+    return signature.toString('base64');
+  }
+  sign() {
+    const message = this.signFormGroup.controls['messageCtrl'].value;
+
+    //const privateKey = null;
+    const signature = this.signData(null, null, message);
   }
 }
