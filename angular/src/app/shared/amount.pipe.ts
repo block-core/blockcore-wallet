917a14cfdb669b2595d23c1759c060d9df6bbdd2
Lu <117081472+missmeowness@users.noreply.github.com>
2022-12-14 17:25:06 +0300

    Display decimals for tokens (#308)
    Closes #300

--- angular/src/app/shared/amount.pipe.ts
+++ angular/src/app/shared/amount.pipe.ts
@@ -6,26 +6,25 @@ import { SATOSHI_FACTOR } from './constants';
 
 @Pipe({ name: 'amount', pure: false })
 export class AmountPipe implements PipeTransform {
+  constructor(public settings: SettingsService) {}
 
-    constructor(public settings: SettingsService) {
+  transform(value: number | Big, decimals = 8, useFormat = true): string {
+    let amountFormat = this.settings.values.amountFormat;
+    const factor = Number('1' + ''.padStart(decimals, '0')) 
 
+    if (amountFormat == 'bitcoin' || useFormat == false) {
+      if (typeof value === 'bigint') {
+        return (Number(value) / factor ).toFixed(decimals);
+      } else {
+        return ((value as number) / factor ).toFixed(decimals);
+      }
+    } else if (amountFormat == 'sat') {
+      return value.toString();
+    } else {
+      const formatted = satcomma.fromSats(value, { validateBitcoinMaxSupply: false });
+      const values = formatted.split('.');
+      return (+values[0]).toLocaleString('en-US', { maximumFractionDigits: 0 }) + '.' + values[1];
     }
 
-    transform(value: number | Big): string {
-        if (this.settings.values.amountFormat == 'sat') {
-            return value.toString();
-        }
-        else if (this.settings.values.amountFormat == 'bitcoin') {
-            if (typeof value === 'bigint') {
-                return (Number(value) / SATOSHI_FACTOR).toFixed(8);
-            } else {
-                return (value as number / SATOSHI_FACTOR).toFixed(8);
-            }
-        }
-        else {
-            const formatted = satcomma.fromSats(value, { validateBitcoinMaxSupply: false });
-            const values = formatted.split('.');
-            return (+values[0]).toLocaleString('en-US', { maximumFractionDigits: 0 }) + '.' + values[1];
-        }
-    }
+  }
 }
