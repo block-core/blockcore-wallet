d397e38faa97b7f43ee58cad8548c257c1063922
SondreB <sondre@outlook.com>
2023-01-21 00:58:09 +0100

    Fix the issue with encryption not using correct public key

--- angular/src/shared/handlers/nostr-encrypt-handler.ts
+++ angular/src/shared/handlers/nostr-encrypt-handler.ts
@@ -22,15 +22,15 @@ export class NostrEncryptHandler implements ActionHandler {
     const { network, node } = await this.backgroundManager.getKey(permission.walletId, permission.accountId, permission.keyId);
 
     const privateKeyHex = node.privateKey as string;
-    const publicKeyHex = getPublicKey(privateKeyHex);
-
+    
     // TODO: Add support for using peer to find existing key, if available! Then read from state.content.peer.
     if (typeof state.content.plaintext !== 'string') {
       state.content.plaintext = JSON.stringify(state.content.plaintext);
     }
-
-    const cipher = await encrypt(privateKeyHex, publicKeyHex, state.content.plaintext);
-
+    
+    const cipher = await encrypt(privateKeyHex, state.message.request.params[0].peer, state.content.plaintext);
+    
+    const publicKeyHex = getPublicKey(privateKeyHex);
     return { key: publicKeyHex, response: cipher };
   }
 }
