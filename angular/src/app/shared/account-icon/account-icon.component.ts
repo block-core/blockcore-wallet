39faf81457775ff8a078a3b0247d75e1eecea29e
SondreB <sondre@outlook.com>
2023-06-29 09:46:53 +0200

    Add option to show larger icon

--- angular/src/app/shared/account-icon/account-icon.component.ts
+++ angular/src/app/shared/account-icon/account-icon.component.ts
@@ -3,11 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-account-icon',
   templateUrl: './account-icon.component.html',
+  styleUrls: ['./account-icon.component.css'],
 })
 export class AccountIconComponent {
   @Input() value: any;
+  @Input() large: boolean = false;
 
   constructor() {}
-
- 
 }
