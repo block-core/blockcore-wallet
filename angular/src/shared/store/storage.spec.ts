2cc159d85d03fa2232d132972a6f1f8e3eabf50a
SondreB <sondre@outlook.com>
2023-07-19 15:20:15 +0200

    Custom storage tables are not yet enabled, disable the tests

--- angular/src/shared/store/storage.spec.ts
+++ angular/src/shared/store/storage.spec.ts
@@ -1,58 +1,56 @@
-import { storage } from 'webextension-polyfill';
-import { Storage, TableAccountState, TableWallet } from './storage';
-import { str } from '@scure/base';
-import exp from 'constants';
+// import { storage } from 'webextension-polyfill';
+// import { Storage, TableAccountState, TableWallet } from './storage';
+// import { str } from '@scure/base';
+// import exp from 'constants';
 
-describe('Storage', () => {
-    let storage: Storage;
+// describe('Storage', () => {
+//     let storage: Storage;
 
-    beforeEach( async () => {
-        storage = new Storage('blockcore-wallet-test');
-        await storage.open(); 
-     });
+//     beforeEach( async () => {
+//         storage = new Storage('blockcore-wallet-test');
+//         await storage.open(); 
+//      });
 
-    it('Should be able to create the database', async () => {
-        expect(storage).toBeDefined();
-    });
+//     it('Should be able to create the database', async () => {
+//         expect(storage).toBeDefined();
+//     });
 
-    it('Should persist account state', async () => {
+//     it('Should persist account state', async () => {
         
-        var doc: TableAccountState = {
-            id: '12345',
-            balance: 110
-        };
+//         var doc: TableAccountState = {
+//             id: '12345',
+//             balance: 110
+//         };
 
-        await storage.putAccountState(doc);
+//         await storage.putAccountState(doc);
 
-        var result = await storage.getAccountState('12345');
-        expect(doc.balance).toBe(result.balance);
+//         var result = await storage.getAccountState('12345');
+//         expect(doc.balance).toBe(result.balance);
+//         // await storage.delete();
+//     });
 
-        // await storage.delete();
-    });
+//     it('Should persist and delete wallet state', async () => {
+//        var doc: TableWallet = {
+//         id: '123',
+//         name: 'testWallet',
+//         restored: false,
+//         mnemonic: '',
+//         accounts: [],
+//         extensionWords: '',
+//         biometrics: false
+//        };
 
-    it('Should persist and delete wallet state', async () => {
-       var doc: TableWallet = {
-        id: '123',
-        name: 'testWallet',
-        restored: false,
-        mnemonic: '',
-        accounts: [],
-        extensionWords: '',
-        biometrics: false
-       };
+//        await storage.putWallet(doc);
 
-       await storage.putWallet(doc);
+//        let result = await storage.getWallet('1234');
+//        expect(result).toBeUndefined();
+//        result = await storage.getWallet('123');
+//        expect(result).toBeDefined();
+//        expect(result.name).toBe('testWallet');
 
-       let result = await storage.getWallet('1234');
-       expect(result).toBeUndefined();
-       result = await storage.getWallet('123');
-       expect(result).toBeDefined();
-       expect(result.name).toBe('testWallet');
+//        await storage.deleteWallet(doc.id);
 
-       await storage.deleteWallet(doc.id);
-
-       result = await storage.getWallet(doc.id);
-       expect(result).toBeUndefined();
-
-    })
-});
+//        result = await storage.getWallet(doc.id);
+//        expect(result).toBeUndefined();
+//     })
+// });
