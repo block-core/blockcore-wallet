85fe36081e99a602ae2df1d7bf8e3d67a4181113
tirth2004 <tirth.bhayani.cse22@itbhu.ac.in>
2024-08-28 15:38:52 +0530

    PSBT support completed

--- angular/src/shared/handlers/sign-psbt-handler.ts
+++ angular/src/shared/handlers/sign-psbt-handler.ts
@@ -1,61 +1,49 @@
 import { BackgroundManager } from '../background-manager';
-import { ActionMessage, ActionPrepareResult, ActionRequest, ActionResponse, Actions, Permission } from '../interfaces';
+import { ActionPrepareResult, ActionResponse, Permission } from '../interfaces';
 import { ActionHandler, ActionState } from './action-handler';
-import * as bitcoinMessage from 'bitcoinjs-message';
 import { HDKey } from '@scure/bip32';
 import { Network } from '../networks';
 import { Psbt } from '@blockcore/blockcore-js';
-import * as bip32 from '@scure/bip32';
 import BIP32Factory from 'bip32';
 import ecc from '@bitcoinerlab/secp256k1';
 const Bip32 = BIP32Factory(ecc);
+import { bip32 } from '../noble-ecc-wrapper';
 
-const testHDKey = "tprv8ZgxMBicQKsPd9TeAdPADNnSyH9SSUUbTVeFszDE23Ki6TBB5nCefAdHkK8Fm3qMQR6sHwA56zqRmKmxnHk37JkiFzvncDqoKmPWubu7hDF";
+// const testHDKey = "tprv8ZgxMBicQKsPf5D3jKXvvvsRi3RMZvE8g98752W3KeCUeggbyf8HQ3BJjppWRrHVPhkgKefZZD8x1jCQSHkoSLaagVNWPBfgTtJVhaRewR5";
 
 export class SignPsbtHandler implements ActionHandler {
     action = ['signPsbt'];
 
     constructor(private backgroundManager: BackgroundManager) { }
 
-    async signData(network: Network, node: HDKey, content: string): Promise<string> {
-        // TODO: Investigate if Paul Miller or someone else implements an message signing library relying on noble packages.
-        var signature = bitcoinMessage.sign(content, Buffer.from(node.privateKey), true, network.messagePrefix);
-        console.log("HD KEY: ", node);
-        return signature.toString('base64');
-    }
-
     private hexToBase64(hex: string): string {
         const buffer = Buffer.from(hex, 'hex');
         return buffer.toString('base64');
     }
 
-    async signPsbt(tnetwork: Network, node: HDKey, content: string): Promise<string> {
-        // console.log("Yes bhai I am signing this PSBT");
+    async signPsbt(network: Network, content: string, xPrivKey: string): Promise<string> {
+        // console.debug("PSBT Handler has been called");
         var hexPsbt = content;
         const psbtBase64 = this.hexToBase64(hexPsbt);
-        const network = {
-            messagePrefix: "\u0018Bitcoin Signed Message:\n",
-            bech32: "tb",
-            bip32: {
-                public: 70617039,
-                private: 70615956,
-            },
-            pubKeyHash: 111,
-            scriptHash: 196,
-            wif: 239,
-        };
+        // const network = {
+        //     messagePrefix: "\u0018Bitcoin Signed Message:\n",
+        //     bech32: "tb",
+        //     bip32: {
+        //         public: 70617039,
+        //         private: 70615956,
+        //     },
+        //     pubKeyHash: 111,
+        //     scriptHash: 196,
+        //     wif: 239,
+        // };
         const psbt = Psbt.fromBase64(psbtBase64, {
             network,
         });
-        console.log("psbt: ", psbt);
-
-        const hdKeyPair = Bip32.fromBase58(testHDKey, network);
-        console.log("HD key Pair: ", hdKeyPair);
-
-
+        // console.log("psbt: ", psbt);
+        const hdKeyPair = Bip32.fromBase58(xPrivKey, network);
+        // console.log("HD key Pair: ", hdKeyPair);
         const SignedPsbt = psbt.signAllInputsHD(hdKeyPair);
-
-        console.log(SignedPsbt);
+        // console.log(SignedPsbt);
 
         return SignedPsbt.toBase64();
     }
@@ -74,7 +62,13 @@ export class SignPsbtHandler implements ActionHandler {
 
     async execute(state: ActionState, permission: Permission): Promise<ActionResponse> {
         // Get the private key
-        const { network, node } = await this.backgroundManager.getKey(permission.walletId, permission.accountId, permission.keyId);
+        const { node, network } = await this.backgroundManager.getKey(permission.walletId, permission.accountId, permission.keyId);
+        // console.log("HD Key pair in Blockcore wallet: ", node);
+        const masterSeedBase64 = await this.backgroundManager.getMasterSeedBase64(permission.walletId)
+        const masterSeed = Buffer.from(masterSeedBase64, 'base64');
+        const hdNode = bip32.fromSeed(masterSeed, network);
+        const xPrivKey = hdNode.toBase58();
+
         if (state.content) {
             let contentText = state.content;
 
@@ -82,7 +76,7 @@ export class SignPsbtHandler implements ActionHandler {
                 contentText = JSON.stringify(state.content);
             }
 
-            let signedData = await this.signPsbt(network, node, contentText as string);
+            let signedData = await this.signPsbt(network, contentText as string, xPrivKey);
 
             return { key: permission.key, walletId: permission.walletId, accountId: permission.accountId, response: { signature: signedData, content: state.content }, network: network.id };
         } else {
